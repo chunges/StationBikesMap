b0ff27498bee75afb7ee1c01d54f80f7ce01dd4d
chunges <chungengsim@gmail.com>
2015-05-08 20:29:58 +0200

    Google Map
    Retrieve location data from Azure SQL databases and display on the
    website using GoogleMap.

---
+++ StationBikesMap_Droid/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.App; 
+using Android.Support.V4.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model; 
+
+
+namespace StationBikesMap_Droid
+{
+    [Activity(Label = "StationBikesMap_Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity,IOnMapReadyCallback
+    {
+        private GoogleMap mMap; 
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            SetUpMap(); 
+        }       
+
+        private void SetUpMap()
+        {
+            if (mMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this); 
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            mMap = googleMap; 
+        }
+    }
+}
+
